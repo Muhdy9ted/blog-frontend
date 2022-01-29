a917a9df9a6234b4de5cef926f7d01843dbb9ce2
Muhammed Abdullahi <MABDULLAHI@unionbankng.com>
2022-01-29 20:50:04 +0100

    added error boundary component/library for error handling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { createContext, useState, useMemo } from 'react';
 import { withRouter } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from "@mui/material/CssBaseline";
+import { ErrorBoundary } from 'react-error-boundary';
 
 import routes from './routes';
 import getDesignTokens from './theme';
+import Fallback from './webpages/fallback';
 
 export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
@@ -22,11 +24,17 @@ function App() {
 
   const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
 
+  const errorHandler = (error, errorInfo) => {
+    console.log('logging', error, errorInfo);
+  }
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline />   
-        {routes}
+        <CssBaseline />
+        <ErrorBoundary fallback={Fallback} onError={errorHandler}>
+          {routes}
+        </ErrorBoundary> 
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
--- src/webpages/homepage.js
+++ src/webpages/homepage.js
@@ -5,12 +5,20 @@ import IconButton from '@mui/material/IconButton';
 import { useTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { useErrorHandler } from 'react-error-boundary';
 
 import {ColorModeContext}  from '../App';
 
 export default function Homepage() {
     const theme = useTheme();
     const colorMode = React.useContext(ColorModeContext);
+    const handleError = useErrorHandler();
+
+    // try{
+
+    // }catch(e){
+    //     handleError(e)
+    // }
     
     return (
         <div>
